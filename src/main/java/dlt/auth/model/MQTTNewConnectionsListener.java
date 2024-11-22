a052f4cee4632861557d61619b7c54109ed34e41
Uellington Damasceno <uellington99@gmail.com>
2024-11-22 15:48:31 -0300

    If applied, this commit will add new logs.

--- src/main/java/dlt/auth/model/MQTTNewConnectionsListener.java
+++ src/main/java/dlt/auth/model/MQTTNewConnectionsListener.java
@@ -6,7 +6,6 @@ import br.uefs.larsid.extended.mapping.devices.services.IDevicePropertiesManager
 import br.uefs.larsid.extended.mapping.devices.tatu.DeviceWrapper;
 import br.uefs.larsid.extended.mapping.devices.tatu.ExtendedTATUWrapper;
 import br.ufba.dcc.wiser.soft_iot.entities.Device;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,6 +23,8 @@ public class MQTTNewConnectionsListener implements MqttCallback {
 
     private ClientMqttManager clientMqtt;
     private IDevicePropertiesManager deviceManager;
+    private static final Logger logger = Logger.getLogger(MQTTNewConnectionsListener.class.getName());
+
 
     public void setClientMqtt(ClientMqttManager client) {
         this.clientMqtt = client;
@@ -35,6 +36,7 @@ public class MQTTNewConnectionsListener implements MqttCallback {
 
     public void initialize() throws MqttException {
         this.clientMqtt.setCallback(this);
+        logger.log(Level.INFO, "SOFT-IOT-DLT-AUTH - Listener topic: {0}", ExtendedTATUWrapper.getConnectionTopic());
         this.clientMqtt.subscribe(ExtendedTATUWrapper.getConnectionTopic());
     }
 
@@ -44,22 +46,25 @@ public class MQTTNewConnectionsListener implements MqttCallback {
 
     @Override
     public void connectionLost(Throwable cause) {
-        Logger.getLogger(MQTTNewConnectionsListener.class.getName()).log(Level.SEVERE, null, cause);
+        logger.log(Level.SEVERE, null, cause);
     }
 
     @Override
     public void messageArrived(String topic, MqttMessage message) throws Exception {
         TATUMessage tatuMessage = new TATUMessage(message.getPayload());
         if (tatuMessage.getMethod().equals(ExtendedTATUMethods.CONNECT)) {
+
             Map sDevice = new JSONObject(tatuMessage.getMessageContent())
                     .getJSONObject("DEVICE").toMap();
 
             Device device = DeviceWrapper.toDevice(sDevice);
+            logger.log(Level.INFO, "SOFT-IOT-DLT-AUTH - recive connect message from {0}", device.getId());
 
             String connackMessage = ExtendedTATUWrapper
                     .buildConnackMessage(device.getId(), device.getId(), true);
             this.deviceManager.addDevice(device);
             String targetTopic = ExtendedTATUWrapper.getConnectionTopicResponse();
+            logger.log(Level.INFO, "SOFT-IOT-DLT-AUTH - SEND: {0} to {1}", new String[]{connackMessage, targetTopic});
             this.clientMqtt.publish(targetTopic, connackMessage);
         }
     }
